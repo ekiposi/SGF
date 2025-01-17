2cfc29cfdf1fe26b0879751186468feb1f3b3243
Jyvor Slaadel Mortimer <mortimerjyvor@gmail.com>
2025-01-17 12:33:45 -0500

    d

--- app.py
+++ app.py
@@ -1265,4 +1265,4 @@ def run_backup_scheduler():
 if __name__ == '__main__':
     import threading
     threading.Thread(target=run_backup_scheduler).start()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
